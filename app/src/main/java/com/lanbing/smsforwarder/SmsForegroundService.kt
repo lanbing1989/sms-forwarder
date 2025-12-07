55c8ae2142e817b96a624e322d19332cacad7b8e
灯火通明 <lanbing89@gmail.com>
2025-12-07 16:02:16 +0800

    Update SmsForegroundService.kt

--- app/src/main/java/com/lanbing/smsforwarder/SmsForegroundService.kt
+++ app/src/main/java/com/lanbing/smsforwarder/SmsForegroundService.kt
@@ -8,6 +8,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
