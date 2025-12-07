a005fa2e104a67f8026fb371e01cddce3fa573de
灯火通明 <lanbing89@gmail.com>
2025-12-07 15:08:39 +0800

    Update MainActivity.kt

--- app/src/main/java/com/lanbing/smsforwarder/MainActivity.kt
+++ app/src/main/java/com/lanbing/smsforwarder/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
